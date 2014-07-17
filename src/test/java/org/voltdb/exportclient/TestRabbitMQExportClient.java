e1326d557145628439a7f9726ba65b7be97a8526
Ning Shi <nshi@voltdb.com>
2014-07-17 14:31:48 -0400

    ENG-6540: Use base class in RabbitMQ export tests.

--- src/test/java/org/voltdb/exportclient/TestRabbitMQExportClient.java
+++ src/test/java/org/voltdb/exportclient/TestRabbitMQExportClient.java
@@ -23,76 +23,24 @@
 
 package org.voltdb.exportclient;
 
-import com.google_voltpatches.common.collect.Lists;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 import org.junit.Before;
 import org.junit.Test;
 import org.voltdb.VoltDB;
-import org.voltdb.VoltTable;
-import org.voltdb.VoltType;
 import org.voltdb.export.AdvertisedDataSource;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Properties;
 import java.util.TimeZone;
 
 import static org.junit.Assert.*;
 
-public class TestRabbitMQExportClient {
-    static final String[] COLUMN_NAMES = {"tid", "ts", "sq", "pid", "site", "op",
-            "tinyint", "smallint", "integer", "bigint", "float", "timestamp", "string", "decimal"};
-
-    static final VoltType[] COLUMN_TYPES
-            = {VoltType.BIGINT, VoltType.BIGINT, VoltType.BIGINT, VoltType.BIGINT, VoltType.BIGINT, VoltType.BIGINT,
-            VoltType.TINYINT, VoltType.SMALLINT, VoltType.INTEGER,
-            VoltType.BIGINT, VoltType.FLOAT, VoltType.TIMESTAMP,VoltType.STRING, VoltType.DECIMAL};
-
-    static final Integer COLUMN_LENGTHS[] = {
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 0
-    };
-
-    static VoltTable vtable = new VoltTable(
-            new VoltTable.ColumnInfo("VOLT_TRANSACTION_ID", VoltType.BIGINT),
-            new VoltTable.ColumnInfo("VOLT_EXPORT_TIMESTAMP", VoltType.BIGINT),
-            new VoltTable.ColumnInfo("VOLT_EXPORT_SEQUENCE_NUMBER", VoltType.BIGINT),
-            new VoltTable.ColumnInfo("VOLT_PARTITION_ID", VoltType.BIGINT),
-            new VoltTable.ColumnInfo("VOLT_TRANSACTION_ID", VoltType.BIGINT),
-            new VoltTable.ColumnInfo("VOLT_SITE_ID", VoltType.BIGINT),
-            new VoltTable.ColumnInfo("tinyint", VoltType.TINYINT),
-            new VoltTable.ColumnInfo("smallint", VoltType.SMALLINT),
-            new VoltTable.ColumnInfo("integer", VoltType.INTEGER),
-            new VoltTable.ColumnInfo("bigint", VoltType.BIGINT),
-            new VoltTable.ColumnInfo("float", VoltType.FLOAT),
-            new VoltTable.ColumnInfo("timestamp", VoltType.TIMESTAMP),
-            new VoltTable.ColumnInfo("string", VoltType.STRING),
-            new VoltTable.ColumnInfo("decimal", VoltType.DECIMAL)
-    );
-
-    static AdvertisedDataSource constructTestSource(boolean replicated, int partition)
-    {
-        ArrayList<String> col_names = new ArrayList<String>();
-        ArrayList<VoltType> col_types = new ArrayList<VoltType>();
-        for (int i = 0; i < COLUMN_TYPES.length; i++)
-        {
-            col_names.add(COLUMN_NAMES[i]);
-            col_types.add(COLUMN_TYPES[i]);
-        }
-        String partCol = replicated ? null : "smallint";
-        //clear the table
-        vtable.clearRowData();
-        AdvertisedDataSource source = new AdvertisedDataSource(partition, "foo", "yankeelover",
-                partCol, 0, 32, col_names, col_types, Arrays.asList(COLUMN_LENGTHS),
-                AdvertisedDataSource.ExportFormat.FOURDOTFOUR);
-        return source;
-    }
-
+public class TestRabbitMQExportClient extends ExportClientTestBase {
     @Before
     public void setup()
     {
-        vtable.clearRowData();
+        super.setup();
     }
 
     @Test
